2f6547a095e413abd33485c5fb6d49c3adda6ad0
brizism <montielbriseida@gmail.com>
2018-02-21 23:28:37 -0500

    instatiate classes and make api call

--- js/app.js
+++ js/app.js
@@ -1,6 +1,31 @@
 // Init Word
 const word = new Word;
+// Init UI
+ const ui = new UI;
 
-word.getWords('example')
-  .then(data => console.log(data))
-  .catch(err => console.log(err))
+// Search form and input
+const searchForm = document.getElementById('search-form');
+const searchInput = document.getElementById('search-input');
+
+// Form event listener
+searchForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  // Get search word
+  const searchWord = searchInput.value;
+  console.log(searchWord)
+
+  if(searchWord !== ''){
+    // Make http call
+    word.getWords(searchWord)
+      .then(data => data.message === 'word not found' ? console.log('word not found') : ui.paint(data.wordData))
+      .catch(err => console.log(err))
+  } else {
+    // Clear words
+    ui.clearWord();
+  }
+
+  // Clear input
+  searchInput.value = '';
+})
+
